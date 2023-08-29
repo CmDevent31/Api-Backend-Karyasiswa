57361fcce0d72ede07a338b6cdb9086c44fa6138
Touzen Aima <touzenkira@gmail.com>
2023-08-29 10:23:07 +0700

    Menyetting AuthController untuk registrasi dan login

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,4 +65,11 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+        'auth:api' => \Illuminate\Auth\Middleware\Authenticate::class,
+        // ...
+    ];
+    
 }
