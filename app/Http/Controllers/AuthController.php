cf0ed0663b189aeb75ca8cc0812a9fec6b15375d
Touzen Aima <touzenkira@gmail.com>
2023-09-19 10:32:14 +0700

    memperbaiki kernel dan auth controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,14 +7,10 @@ use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Storage;
-
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Exceptions\JWTException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
@@ -42,21 +38,19 @@ class AuthController extends Controller
 
             // Cek kredensial dan buat token
             $credentials = $request->only('email', 'password');
-            $user = User::where('email', $request->email)->first(); // Cek apakah email ditemukan
+            $user = User::where('email', $request->email)->first();
 
             if (!$user) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Email tidak ditemukan',
-                    'data' => [],
-                ], 401);
+                ], 404);
             }
 
             if (!$token = JWTAuth::attempt($credentials)) {
                 return response()->json([
                     'status' => 'error',
                     'message' => 'Password salah',
-                    'data' => [],
                 ], 401);
             }
 
@@ -64,11 +58,15 @@ class AuthController extends Controller
             $user = auth()->user();
 
             return $this->createNewToken($token, $user);
+        } catch (JWTException $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Terjadi kesalahan saat mencoba membuat token',
+            ], 500);
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Terjadi kesalahan internal server',
-                'data' => (object)[],
+                'message' => 'Terjadi kesalahan internal server: ' . $e->getMessage(),
             ], 500);
         }
     }
@@ -87,7 +85,7 @@ class AuthController extends Controller
                 'dob' => 'required|date|max:255',
                 'bio' => 'required|string|max:255',
                 'phone_number' => 'required|string|max:14',
-            ]);
+                ]);
 
             if ($validator->fails()) {
                 return response()->json([
@@ -109,15 +107,6 @@ class AuthController extends Controller
             $user->bio = $request->input('bio');
             $user->phone_number = $request->input('phone_number');
 
-            if ($request->hasFile('profile_image')) {
-                $image = $request->file('profile_image');
-                $imagePath = 'uploads/' . time() . '_' . Str::slug(pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME)) . '.' . $image->getClientOriginalExtension();
-
-                Storage::disk('public')->put($imagePath, file_get_contents($image));
-
-                $user->profile_image = url(Storage::url($imagePath));
-            }
-
             $user->save();
 
             $token = JWTAuth::fromUser($user);
@@ -126,28 +115,28 @@ class AuthController extends Controller
         } catch (\Exception $e) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'Terjadi kesalahan internal server',
-                'data' => (object)[],
+                'message' => 'Terjadi kesalahan internal server: ' . $e->getMessage(),
             ], 500);
         }
     }
-    public function update(Request $request, $id)
-    {
-        try {
-            //1. Find the user to update
-            $userToUpdate = User::findOrFail($id);
-    
-            // 2. Check if the current user can update the specified user (you can add your own authorization logic here)
-            if (auth()->user()->cannot('update', $userToUpdate)) {
-                return response()->json([
-                    'status' => 'error',
-                    'message' => 'Unauthorized to update this user',
-                    'data' => (object)[],
-                ], 403);
+
+             public function update(Request $request, $id)
+            {
+            try {
+//1. Find the user to update
+                $userToUpdate = User::findOrFail($id);
+
+// 2. Check if the current user can update the specified user (you can add your own authorization logic here)
+    if (auth()->user()->cannot('update', $userToUpdate)) {
+    return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized to update this user',
+                'data' => (object)[],
+        ], 403);
             }
 
-            // 3. Validate the request data
-            $validator = Validator::make($request->all(), [
+// 3. Validate the request data
+                $validator = Validator::make($request->all(), [
                 'email' => 'sometimes|required|email|max:255|unique:users,email,' . $userToUpdate->id,
                 'password' => 'sometimes|required|min:6',
                 'profile_image' => 'sometimes|image|mimes:jpeg,png,jpg,gif|max:20480',
@@ -156,103 +145,103 @@ class AuthController extends Controller
                 'dob' => 'sometimes|required|max:255',
                 'bio' => 'sometimes|required|max:255',
                 'phone_number' => 'sometimes|required|max:14',
-            ]);
-    
-            // 4. If validation fails, return an error response
-            if ($validator->fails()) {
+                ]);
+
+                // 4. If validation fails, return an error response
+                if ($validator->fails()) {
                 return response()->json([
-                    'status' => 'error',
-                    'message' => 'Validation failed',
-                    'errors' => $validator->errors()
+                'status' => 'error',
+                'message' => 'Validation failed',
+                'errors' => $validator->errors()
                 ], 400);
-            }
-    
-            // 5. Update user data based on the request
-            if ($request->has('email')) {
+                }
+
+                // 5. Update user data based on the request
+                if ($request->has('email')) {
                 $userToUpdate->email = $request->input('email');
-            }
-            if ($request->has('password')) {
+                }
+                if ($request->has('password')) {
                 $userToUpdate->password = Hash::make($request->input('password'));
-            }
-            if ($request->has('username')) {
+                }
+                if ($request->has('username')) {
                 $userToUpdate->username = $request->input('username');
-            }
-            if ($request->has('kelas')) {
+                }
+                if ($request->has('kelas')) {
                 $userToUpdate->kelas = $request->input('kelas');
-            }
-            if ($request->has('dob')) {
+                }
+                if ($request->has('dob')) {
                 $userToUpdate->dob = $request->input('dob');
-            }
-            if ($request->hasFile('profile_image')) {
+                }
+                if ($request->hasFile('profile_image')) {
                 $image = $request->file('profile_image');
                 $imagePath = 'uploads/' . time() . '_' . Str::slug(pathinfo($image->getClientOriginalName(), PATHINFO_FILENAME)) . '.' . $image->getClientOriginalExtension();
-    
-                Storage::disk('public')->put($imagePath, file_get_contents($image));
-    
-                $userToUpdate->profile_image = url(Storage::url($imagePath));
-            }
-            if ($request->has('bio')) {
-                $userToUpdate->bio = $request->input('bio');
-            }
-            if ($request->has('phone_number')) {
-                $userToUpdate->phone_number = $request->input('phone_number');
-            }
-    
-            // 6. Save the updated user data
-            $userToUpdate->save();
-    
-            // 7. Return a success response
-            return response()->json([
-                'status' => 'success',
-                'message' => 'User data updated successfully',
-                'data' => $userToUpdate,
-            ], 200);
-        } catch (\Exception $e) {
-            // 8. Handle internal server errors
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Internal server error: ' . $e->getMessage(),
-                'data' => (object)[],
-            ], 500);
-        }
-    }
-    
 
-    
+                Storage::disk('public')->put($imagePath, file_get_contents($image));
+
+                $userToUpdate->profile_image = url(Storage::url($imagePath));
+                }
+                if ($request->has('bio')) {
+                $userToUpdate->bio = $request->input('bio');
+                }
+                if ($request->has('phone_number')) {
+                $userToUpdate->phone_number = $request->input('phone_number');
+                }
+
+                // 6. Save the updated user data
+                $userToUpdate->save();
+
+                // 7. Return a success response
+                        return response()->json([
+                        'status' => 'success',
+                        'message' => 'User data updated successfully',
+                        'data' => $userToUpdate,
+                ], 200);
+                } catch (\Exception $e) {
+                // 8. Handle internal server errors
+                return response()->json([
+                        'status' => 'error',
+                        'message' => 'Internal server error: ' . $e->getMessage(),
+                        'data' => (object)[],
+                ], 500);
+                }
+                }
+
+
+
 
     public function getUserInfo()
-    {
-        $user = auth()->user();
+                {
+                    $user = auth()->user();
 
-        return response()->json([
-            'status' => 'success',
-            'data' => $user,
-        ]);
-    }
+                    return response()->json([
+                    'status' => 'success',
+                    'data' => $user,
+                ]);
+                }
 
-    public function logout()
-    {
-        Auth::logout();
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Successfully logged out',
-            'data' => (object)[],
-        ]);
-    }
+        public function logout()
+                {
+                        Auth::logout();
+                        return response()->json([
+                        'status' => 'success',
+                        'message' => 'Successfully logged out',
+                        'data' => (object)[],
+                ]);
+                }
 
     public function refresh()
-    {
-        return response()->json([
-            'status' => 'success',
-            'data' => Auth::user(),
-            'authorization' => [
-                'token' => Auth::refresh(),
-                'type' => 'bearer',
-            ]
-        ]);
-    }
+                {
+                return response()->json([
+                        'status' => 'success',
+                        'data' => Auth::user(),
+                        'authorization' => [
+                        'token' => Auth::refresh(),
+                        'type' => 'bearer',
+                ]
+                ]);
+                }
 
-    protected function createNewToken($token)
+    protected function createNewToken($token, $user)
     {
         return response()->json([
             'status' => 'success',
@@ -260,7 +249,7 @@ class AuthController extends Controller
             'access_token' => $token,
             'token_type' => 'bearer',
             'expires_in' => JWTAuth::factory()->getTTL() * 60,
-            'user' => auth()->user()
+            'user' => $user,
         ]);
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array<int, class-string|string>
      */
     protected $middleware = [
-        // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // ...
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
@@ -48,7 +47,6 @@ class Kernel extends HttpKernel
             'auth:api',
             \Illuminate\Auth\Middleware\Authenticate::class,
         ],
-        
     ];
 
     /**
@@ -75,7 +73,6 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
         'auth:api' => \Illuminate\Auth\Middleware\Authenticate::class,
-        // ...
+        // ... Middleware rute lainnya.
     ];
-    
 }
