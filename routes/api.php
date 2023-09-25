14b86484581f057e4935df479cb96a997df4ecbb
Touzen Aima <touzenkira@gmail.com>
2023-09-25 09:07:42 +0700

    perbaiki

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\TableCategoryController;
 |
 */
 
-Route::controller(AuthController::class)->group(function () {
+Route::middleware('auth:api')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
     Route::post('logout', [AuthController::class, 'logout']);
