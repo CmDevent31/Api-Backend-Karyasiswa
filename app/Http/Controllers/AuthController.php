e79a17750db3af16b552a25c5d896bcfb23a0497
Touzen Aima <touzenkira@gmail.com>
2023-09-25 10:10:57 +0700

    nyoba-nyoba

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['login', 'register','update','logout']]);
+        $this->middleware('auth:api', ['except' => ['login', 'register','logout']]);
     }
 
     public function login(Request $request)
